c2e83540390acfa28b69a09a7aaccaec670bf3e3
Myzle <myzle@yeah.net>
2016-06-27 06:21:00 +0800

    init project

---
+++ Shui/Shui/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Shui
+//
+//  Created by HeBin on 16/6/27.
+//  Copyright © 2016年 Myzle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Shui/Shui/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Shui
+//
+//  Created by HeBin on 16/6/27.
+//  Copyright © 2016年 Myzle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
